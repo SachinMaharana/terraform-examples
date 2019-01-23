a4103d68a1fd763d889828f120fecf730c725973
SachinMaharana <sachin.nicky@gmail.com>
2019-01-23 10:26:07 +0530

    tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,19 @@
+image = {
+  dev  = "sachinnicky/go-app:terra"
+  prod = "sachinnicky/go-app:latest"
+}
+
+container_name = {
+  dev  = "dev_go_app"
+  prod = "prod_go_app"
+}
+
+# int_port = {
+#   dev  = "8080"
+#   prod = "80"
+# }
+
+ext_port = {
+  dev  = "8080"
+  prod = "80"
+}
--- variables.tf
+++ variables.tf
@@ -6,20 +6,20 @@ variable "image" {
   description = "Image for container"
   type        = "map"
 
-  default = {
-    dev  = "sachinnicky/go-app:terra"
-    prod = "sachinnicky/go-app:latest"
-  }
+  #   default = {
+  #     dev  = "sachinnicky/go-app:terra"
+  #     prod = "sachinnicky/go-app:latest"
+  #   }
 }
 
 variable "container_name" {
   description = "Name of container"
   type        = "map"
 
-  default = {
-    dev  = "dev_go_app"
-    prod = "prod_go_app"
-  }
+  #   default = {
+  #     dev  = "dev_go_app"
+  #     prod = "prod_go_app"
+  #   }
 }
 
 variable "int_port" {
@@ -31,8 +31,8 @@ variable "ext_port" {
   description = "External Port to the image"
   type        = "map"
 
-  default = {
-    dev  = "8080"
-    prod = "80"
-  }
+  #   default = {
+  #     dev  = "8080"
+  #     prod = "80"
+  #   }
 }
