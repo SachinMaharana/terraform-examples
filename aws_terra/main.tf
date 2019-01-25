84e4f0904c44a8250e98463ae645ba7ac3dd16da
SachinMaharana <sachin.nicky@gmail.com>
2019-01-26 00:17:11 +0530

    aws_terraform_root

--- aws_terra/main.tf
+++ aws_terra/main.tf
@@ -6,3 +6,12 @@ module "storage" {
   source       = "./storage"
   project_name = "${var.project_name}"
 }
+
+# Deploying network
+
+module "netwworking" {
+  source = "./nerworking"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip = "${var.accessip}"
+}
